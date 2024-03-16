c2c7db74eca5d4262c129601cc30fbc0318bc1b9
Nick Lane <Nick.Lane5896@gmail.com>
2024-03-16 16:13:26 -0600

    so many changes, not ready to run, yolo

--- miniBot.py
+++ miniBot.py
@@ -10,13 +10,28 @@ class Result:
         self.date = date
         self.time = time
     def __str__(self):
-        return f"Date:{self.date}, time: {self.time}"
+        return f'Date:{self.date}, time: {self.time}'
 
-
+# preference types:
+# no_congrats
+# no_rekkening
+# no_leaderboard
+# goofy_ratio
 class Preference:
-    def __init__(self,type: str, value: int):
+    def __init__(self, type: str, value: int):
         self.type = type
         self.value = value
+
+# Command class. 
+        # type: 'user' | 'admin' 
+        # content: just the command (no 'minibot' or 'mb')
+        # user: the discord User executing the command. 
+class Command:
+    def __init__(self, type: str, content: str, user: discord.User, message: discord.Message):
+        self.type = type
+        self.content = content
+        self.user = user
+        self.message = message
 
 class MbUser:
     def __init__(self, id: discord.user):
@@ -25,32 +40,42 @@ class MbUser:
         self.preferences = [] # list of type Preference
         self.times_placed = [] # number of times placed 1st, 2nd, etc. index 0 will be empty so index 1 is 1st, etc.
 
-    def add(self, res: Result, chn: discord.channel):
+    async def add(self, res: Result, chn: discord.channel):
         self.results.append(res)
         for i in self.results:
             if i.date < date.today():
                 self.results.remove(i)
-        self.congratulate(chn)
+        await self.congratulate(chn)
 
-    def get_preference(self, pref_type: str) -> Preference:
+    def get_preference(self, pref_type: str) -> Preference | None:
         for p in self.preferences:
             if p.type == pref_type:
-                return True
-        return False
+                return p
+        return None
     
-    def congratulate(self, chnl: discord.channel):
-        file_name = "files/congratsMessages"
+    def clear_preference(self, pref_type: str):
+        self.preferences = [pref for pref in self.preferences if pref.type != pref_type]
+
+    # TODO goofy ratio 1-10   
+    def set_preference(self, preference: Preference):
+        # if they already have the preference, remove it
+        if self.get_preference(preference.type):
+            self.clear_preference(preference.type)
+        self.preferences.add(preference)
+
+    async def congratulate(self, chnl: discord.TextChannel):
+        file_name = 'files/congratsMessages'
         # if the player has specified a ratio for how often they want to see goofy congratulation messages
-        if self.has_preference("goofy_ratio"):
+        if self.has_preference('goofy_ratio'):
             # if they roll more than their ratio, they get a normal message.
             # if they roll less than or equal to their ratio, they get a goofy one. IDK, this made sense when I wrote it
             if random.randRange(1,10) <= self.goofy_ratio:
-                file_name = "files/goofyCongratsMessages"
-        file = open(file_name, "r")
+                file_name = 'files/goofyCongratsMessages'
+        file = open(file_name, 'r')
         congrats_messages = file.readlines()
         file.close()
         # send a random congratulation message from the appropriate list to the appropriate channel
-        chnl.send(congrats_messages[random.randrange(0,len(congrats_messages))])
+        await chnl.send(congrats_messages[random.randrange(0,len(congrats_messages))])
     
     def place(self, pl):
         # if the list isn't long enough to accept the place given, extend it the proper length
@@ -75,42 +100,28 @@ class Placing:
 
 class Permissions:
     def __init__(self,fname: str):
-        self.p = []
-        file = open(fname, "r")
-        self.p = file.readlines()
+        self.permissions = []
+        file = open(fname, 'r')
+        self.permissions = file.readlines()
         file.close()
     
     # accepts a string, str, which is the permission we're asking for
     # returns a list of strings, which are the usernames of users with that permission, or None
     def get(self, str):
-        for i in self.p:
-            print(i)
+        for i in self.permissions:
             if i.startswith(str):
-                return i[len(str)+1:].split()
+                return i[len(str):].split()
         return None
-
-class preferences:
-    def __init__(self,fname: str):
-        self.p = []
-        file = open(fname, "r")
-        self.p = file.readlines()
-        file.close()
     
-    # accepts a string, str, which is the Preference we're asking for
-    # returns a list of strings, which are the usernames of users with that Preference or None
-    def get(self, str):
-        for i in self.p:
-            print(i)
-            if i.startswith(str):
-                return i[len(str)+1:].split()
-        return None
+    def has_permission(self, name, permission: str) -> bool:
+        return name in self.get(permission)
 
 
 
 class MiniBot:
     def __init__(self):
         self.users = [] # list of type MbUser
-        self.per = Permissions("files/Permissions")
+        self.per = Permissions('files/Permissions')
     
     def is_user(self, userID):
         for u in self.users:
@@ -118,14 +129,14 @@ class MiniBot:
                 return True
         return False
     
-    def get_mb_user(self, userID: discord.user) -> MbUser:
+    def get_mb_user(self, userID: discord.User) -> MbUser | None:
         for u in self.users:
             if u.id == userID:
                 return u
         return None
     
     # add a Result to the user that submitted it
-    def add(self, res: Result, mes: discord.message):
+    def add(self, res: Result, mes: discord.Message):
         # if the submitter isn't already in the system
         userID = mes.author
         if not self.is_user(userID):
@@ -136,11 +147,11 @@ class MiniBot:
 
         
     
-    # Function to return Result object, if the message is a mini Result
-    def check_result(self, message_content) -> Result | None:
+    # return Result object, if the message is a mini Result
+    def check_result(self, message_content: str) -> Result | None:
         # regular expressions to match the patterns
-        url_pattern = r"https://www.nytimes.com/badges/games/mini.html\?d=(\d{4}-\d{2}-\d{2})&t=(\d+)"
-        app_pattern = r"I solved the (\d{1,2}/\d{1,2}/\d{4}) New York Times Mini Crossword in (\d{1,2}):(\d{2})"
+        url_pattern = r'https://www.nytimes.com/badges/games/mini.html\?d=(\d{4}-\d{2}-\d{2})&t=(\d+)'
+        app_pattern = r'I solved the (\d{1,2}/\d{1,2}/\d{4}) New York Times Mini Crossword in (\d{1,2}):(\d{2})'
 
         # match the patterns
         url_match = re.match(url_pattern, message_content)
@@ -148,11 +159,11 @@ class MiniBot:
 
         if url_match:
             date_str = url_match.group(1)
-            date_format = "%Y-%m-%d"
+            date_format = '%Y-%m-%d'
             time = int(url_match.group(2))
         elif app_match:
             date_str = app_match.group(1)
-            date_format = "%m/%d/%Y"
+            date_format = '%m/%d/%Y'
             minutes = int(app_match.group(2))
             seconds = int(app_match.group(3))
             time = minutes * 60 + seconds # convert minutes and seconds to seconds
@@ -164,29 +175,89 @@ class MiniBot:
 
         # create a Result object and return it
         return Result(date,time)
+    # TODO 'say' admin command, so I can make it say whatever I want, in whichever channel I choose
+    # TODO make a 'this' command to identify channels to 
+    async def run_command(self, command: Command):
+        if len(command.content.split()) == 0:
+            return
+        
+        # ------------user commands-----------------------
+        user_commands = ['no_congrats', 'yes_congrats', 'no_rekkening', 'yes_rekkening', 'no_leaderboard', 'yes_leaderboard', 'goofy_ratio']
+        if command.type == 'user':
+            if command.content.split()[0] not in user_commands:
+                await command.message.channel.send('Command not recognized')
+                return
+            else:
+                if command.content.split()[0] == 'goofy_ratio' and len(command.content.split() > 1):
+                    self.get_mb_user(command.user).set_preference(Preference('goofy_ratio', command.content.split()[1]))
+            
+    async def check_command(self, message: discord.Message):
+        possible_command_tokens = ['minibot', 'mb']
+        possible_admin_command_tokens = ['minibot_admin', 'mb_admin']
+        command_token = None
+        command = False
+        admin_command = False
+        for pct in possible_command_tokens:
+            if message.content.lower().startswith(pct):
+                command_token = pct
+                command = True
+        
+        # If it's in my secret admin channel
+        if message.channel.name == 'secret-minibot-commands':
+            command_token = ''
+            command = True
+            admin_command = True
+        # If it's an admin command 
+        for pact in possible_admin_command_tokens:
+            if message.content.lower().startswith(pact):
+                command_token = pact
+                command = True
+                admin_command = True
+        
+        # if it's a command
+        if command:
+            await self.run_command(Command('admin' if admin_command else 'user', message.content[len(command_token):], message.author, message))
+
+        
     
-    def feed(self, message: discord.message):
+        
+        
+    
+    async def feed(self, message: discord.Message):
         r = self.check_result(message.content)
 
         # if it's a Result
         if r is not None:
-            self.add(r, message)
-        # TODO implement commands
-    
-    def daily_leaderboard(self, channel: discord.channel):
-        message = "DAILY LEADERBOARD:\n"
+             await self.add(r, message)
+        
+        # handle commands
+        # TODO COMMANDS
+
+    # returns a string representation of time - seconds:int -> 'm:ss'
+    def format_time(time: int) -> str:
+        if time < 60:
+            return str(time)
+        else:
+            return f'{time // 60}:{time % 60}'
+        
+    def daily_leaderboard(self, channel: discord.TextChannel):
+        message = 'DAILY LEADERBOARD:\n'
         placings = []
         unsorted_placings = []
+
+        # get everyone in an unsorted list of placings
         for u in self.users:
             r = u.get_todays_result()
-            if r is not None:
+            if r and not u.get_preference('no_leaderboard'):
                 unsorted_placings.append(Placing(u,r))
+        # sort the list
         if (len(unsorted_placings) > 0):
             placings = sorted(unsorted_placings, key = lambda x: x.result.time)
-            placings.insert(0,"zero")
+            placings.insert(0,'zero')
+            # give everyone a place number
             i = 1
             while (i < len(placings)):
-                message += f"{i}. {placings[i].user.id.display_name} {placings[i].result.time}\n"
+                message += f'{i}. {placings[i].user.id.display_name} {self.format_time(placings[i].result.time)}\n'
                 placings[i].user.place(i)
                 i += 1
 
@@ -194,14 +265,14 @@ class MiniBot:
             
 
 
-
-
+# TODO handle data so that it won't be lost when disconnecting and/or restarting
+# TODO handle global object properly
 # global object
 bot = MiniBot() #TODO change so it starts as none, then in on_ready, initialize so it can read in JSON and remember everyone's info
 
 class myClient(discord.Client):
     async def on_ready(self):
-        print(f"Logged in as {client.user}")
+        print(f'Logged in as {client.user}')
 
     async def on_message(self, message):
         # Don't respond to your own message
@@ -228,7 +299,7 @@ intents.message_content = True
 client = myClient(intents=intents)
 
 # bot's token
-token_file = open("files/token", "r")
+token_file = open('files/token', 'r')
 token = token_file.read()
 token_file.close()
 
